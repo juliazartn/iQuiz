9e4fd2c158d71c6e0f49a5abbc482b73ce8088fa
juliazartn <julia.zartn@gmail.com>
2018-02-14 03:01:55 -0800

    can choose answer option and receive feedback by the color of selected changing and then click submit to move on

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class QuestionViewController: UIViewController {
     
     var questions : [Question] = []
+    var questionIndex : Int = 0
     var chosenButtonNumber : Int? = nil
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -20,25 +21,28 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var option4Button: UIButton!
     @IBOutlet weak var submitButton: UIButton!
     
+    var allOptionButtons : [UIButton] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.allOptionButtons = [option1Button, option2Button, option3Button, option4Button]
         self.submitButton.isEnabled = false
         
-        var question1 = questions[0]
+        var question1 = questions[questionIndex]
         
         questionLabel.text = question1.question
         question1.answerOptions.append(question1.correctAnswer)
         let allAnswers : [String] = question1.answerOptions
-        var allOptionButtons : [UIButton] = [option1Button, option2Button, option3Button, option4Button]
+        var tempAllOptionButtons : [UIButton] = [option1Button, option2Button, option3Button, option4Button]
         
         //for the four available answers
         for index in 0...3 {
             //pick a random index number between 0 and the number of items in the allOptionButtons array
             //set that random option button to the next answerOption
-            let randomIndex = Int(arc4random_uniform(UInt32(allOptionButtons.count)))
-            allOptionButtons[randomIndex].setTitle(allAnswers[index], for: .normal)
-            allOptionButtons.remove(at: randomIndex)
+            let randomIndex = Int(arc4random_uniform(UInt32(tempAllOptionButtons.count)))
+            tempAllOptionButtons[randomIndex].setTitle(allAnswers[index], for: .normal)
+            tempAllOptionButtons.remove(at: randomIndex)
         }
         
         // Do any additional setup after loading the view.
@@ -46,7 +50,14 @@ class QuestionViewController: UIViewController {
     
     @IBAction func chosenOptionClicked(_ sender : UIButton) {
         self.chosenButtonNumber = sender.tag
-        self.submitButton.isEnabled = trues
+        
+        for index in 0...3 {
+            allOptionButtons[index].setTitleColor(UIColor.black, for: .normal)
+        }
+
+ allOptionButtons[sender.tag].setTitleColor(UIColor.orange , for: .normal)
+        
+        self.submitButton.isEnabled = true
     }
     
     @IBAction func submitClicked(_ sender: Any) {

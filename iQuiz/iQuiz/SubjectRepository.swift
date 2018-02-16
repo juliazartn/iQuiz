9ef250cfe746a2b210ce66c406b8e684687c407c
juliazartn <julia.zartn@gmail.com>
2018-02-16 15:45:06 -0800

    connected subject data to online json

--- iQuiz/iQuiz/SubjectRepository.swift
+++ iQuiz/iQuiz/SubjectRepository.swift
@@ -32,6 +32,58 @@ class Subject {
 class SubjectRepository {
     static let shared = SubjectRepository()
     
+    private var subjects : [Subject]
+    
+    init() {
+        let urlString : String = "https://tednewardsandbox.site44.com/questions.json"
+        let url = NSURL(string: urlString)!
+        let urlRequest = URLRequest(url: url as URL)
+        self.subjects = []
+        
+        let sema = DispatchSemaphore(value: 0);
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) -> Void in
+            if error != nil {
+                print(error ?? "URLSession error")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [[String: AnyObject]]
+                
+                
+                
+                for item in json {
+                    var subjectQ : [Question] = []
+                    
+                    let questionsArray = item["questions"] as? [AnyObject]
+                    for question in questionsArray! {
+                        let questionText = question["text"]
+                        let questionAnswerIndex = (question["answer"] as! NSString).integerValue
+                        var questionAnswers = question["answers"] as? [AnyObject]
+                        let correctAnswer = questionAnswers!.remove(at: questionAnswerIndex-1)
+                        subjectQ.append(Question(question: questionText as! String, correctAnswer: correctAnswer as! String, answerOptions: questionAnswers! as! [String]))
+                    }
+                    
+                    let subjectImageName : String = ((item["title"] as! NSString) as String) + ".jpg"
+                    let subjectImage = UIImage(named: subjectImageName)!
+                    
+                    
+                    self.subjects.append(Subject(subjectTitle: item["title"] as! String, description: item["desc"] as! String, img: subjectImage, questionsArray: subjectQ))
+                }
+            } catch let jsonError {
+                print(jsonError)
+            }
+            sema.signal();
+        }
+        task.resume()
+        sema.wait()
+    }
+    
+    private func createSubjects() -> [Subject]{
+        var webSubjects : [Subject] = []
+        
+        return webSubjects
+    }
+    
     func getSubjects() -> [Subject] {
         return subjects
     }
@@ -40,10 +92,12 @@ class SubjectRepository {
         return subjects[id]
     }
     
-    private let subjects : [Subject] = [
-        Subject(subjectTitle: "Mathematics", description: "numbers and stuff", img: #imageLiteral(resourceName: "math"), questionsArray : [Question(question: "What's 1 + 1", correctAnswer: "2", answerOptions: ["3", "-1", "4"]),
-            Question(question: "What's the derivative of sin(x)?", correctAnswer: "cos(x)", answerOptions: ["tan(x)", "-cos(x)", "sec(x)"])]),
-        Subject(subjectTitle: "Marvel Super Heroes", description: "spiderman's world", img: #imageLiteral(resourceName: "marvel.jpg"), questionsArray : [Question(question: "What's spiderman's real name?", correctAnswer: "Peter Parker", answerOptions: ["Bruce Wayne", "Clark Kent", "Dick Grayson"])]),
-        Subject(subjectTitle: "Science", description: "chemistry, biology, etc", img: #imageLiteral(resourceName: "science.jpg"), questionsArray : [Question(question: "What's entropy?", correctAnswer: "Gradual decline into disorder", answerOptions: ["Heat", "Gradual change to liquid", "Gradual decline into order"])])
-    ]
+    
+//    private let subjects : [Subject] = [
+//        Subject(subjectTitle: "Mathematics", description: "numbers and stuff", img: #imageLiteral(resourceName: "math"), questionsArray : [Question(question: "What's 1 + 1", correctAnswer: "2", answerOptions: ["3", "-1", "4"]),
+//            Question(question: "What's the derivative of sin(x)?", correctAnswer: "cos(x)", answerOptions: ["tan(x)", "-cos(x)", "sec(x)"])]),
+//        Subject(subjectTitle: "Marvel Super Heroes", description: "spiderman's world", img: #imageLiteral(resourceName: "marvel.jpg"), questionsArray : [Question(question: "What's spiderman's real name?", correctAnswer: "Peter Parker", answerOptions: ["Bruce Wayne", "Clark Kent", "Dick Grayson"])]),
+//        Subject(subjectTitle: "Science", description: "chemistry, biology, etc", img: #imageLiteral(resourceName: "science.jpg"), questionsArray : [Question(question: "What's entropy?", correctAnswer: "Gradual decline into disorder", answerOptions: ["Heat", "Gradual change to liquid", "Gradual decline into order"])])
+//    ]
+//
 }
